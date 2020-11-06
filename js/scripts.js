d7be19de4f7be67957911da75bf9efa24dd7b2f9
Nataliya Zhuravleva <natalindria@gmail.com>
2020-11-06 09:02:57 -0800

    Add Pizza, Size, Topping.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 // Business logic:
+function Pizza (){
+  this.sizes=[];
+  this.toppings=[];
+}
 
+function Size(pizzaSize,sizePrice){
+this.pizzaSize=pizzaSize;
+this.sizePrice=sizePrice;
+}
+
+function Topping(toppingName, toppingPrice){
+this.toppingName=toppingName;
+this.toppingPrice=toppingPrice;
+}
+
+let pizza=new Pizza();
 
 
 // User interface logic:
